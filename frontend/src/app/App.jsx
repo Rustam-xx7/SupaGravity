545d8bc3cfb05f1e930ee2c3593157a447b340d9
Rustam-xx7 <chakrabortyrustam@gmail.com>
2026-06-24 01:13:41 +0530

    initiating the project

---
+++ frontend/src/app/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="App h-screen w-full bg-slate-950 flex gap-4 p-4">
+      <aside className="h-full w-1/4 bg-amber-50 rounded-lg"></aside>
+      <section className="h-full w-3/4 bg-neutral-800 rounded-lg"></section>
+    </main>
+  );
+}
+
+export default App;
